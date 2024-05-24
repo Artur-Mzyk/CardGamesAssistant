d5f00379058f73747a961b16c3c8c642b258e65a
adrianponiat <adrianponiat@student.agh.edu.pl>
2024-05-24 02:16:33 +0200

    Add hearts game

---
+++ hearts/agent.py
@@ -0,0 +1,128 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict
+from card import Card
+
+
+class HeartsAgent(ABC):
+    NUM_AGENTS = 4
+
+    """
+    @param agent_id
+    @param cards a list of `Card` corresponding to the cards in the hand of this agent.
+    """
+    @abstractmethod
+    def __init__(self, agent_id, cards):
+        pass
+
+    """
+    @returns action agent would take.
+    """
+    @abstractmethod
+    def getNextAction(self):
+        pass
+
+    """
+    @description called when any agent takes an action (including this one).
+    """
+    @abstractmethod
+    def observeActionTaken(self, agent_id, card):
+        pass
+
+    """
+    @description called when any agent needs to reset seen cards    
+    """
+    @abstractmethod
+    def resetSeenCards(self):
+        pass
+
+
+class SimpleHeartsAgent(HeartsAgent):
+    def __init__(self, agent_id, cards):
+        self.agent_id = agent_id
+        self.in_play = []
+        self.card_map = defaultdict(list)
+        for card in cards:
+            self.card_map[card.suit].append(card)
+
+    def getNextAction(self):
+        # if there's a leading suit, play the last card (for efficiency) with the same suit as the leading suit.
+        if len(self.in_play) > 0:
+            leading_suit = self.in_play[0].suit
+            if len(self.card_map[leading_suit]) > 0:
+                return self.card_map[leading_suit][-1]
+        # choose any remaining card
+        for _, cards in self.card_map.items():
+            if len(cards) > 0:
+                return cards[-1]
+        raise RuntimeError(
+            f"Agent {self.agent_id} does not have any remaining cards to play.")
+
+    def observeActionTaken(self, agent_id, card):
+        if self.agent_id == agent_id:
+            # since this agent's action should be the last element from this list, this should be a constant operation.
+            self.card_map[card.suit].remove(card)
+        self.in_play.append(card)
+        if len(self.in_play) == self.NUM_AGENTS:
+            self.in_play.clear()
+
+    def resetSeenCards(self):
+        pass
+
+
+class MDPHeartsAgent(HeartsAgent):
+    def __init__(self, agent_id, cards, getSeen, get_next_action_fn, observe_action_taken_fn):
+        self.agent_id = agent_id
+        self.in_play = []
+        print(f"Initializing MDPHeartsAgent w/ id {self.agent_id}")
+        for card in cards:
+            print((card.num, card.suit))
+        self.cards = set(cards)
+        self.getSeen = getSeen
+        self.get_next_action_fn = get_next_action_fn
+        self.observe_action_taken_fn = observe_action_taken_fn
+
+    def getNextAction(self):
+        remaining_moves = (self.NUM_AGENTS - 1) - len(self.in_play)
+        leading_suit = Card.Suit.getSuitShortStr(
+            self.in_play[0].suit) if len(self.in_play) > 0 else "none"
+
+        player_cards = []
+        for i in range(Card.NUM_CARDS):
+            card = Card.createFromCardIdx(i)
+            if card in self.in_play:
+                player_cards.append("in_play")
+            elif f"{card.num}{Card.Suit.getSuitShortStr(card.suit)}" in self.getSeen():
+                player_cards.append("seen")
+            elif card in self.cards:
+                player_cards.append("m1")
+                continue
+            else:
+                player_cards.append("m2")
+        action_str = self.get_next_action_fn(
+            remaining_moves, leading_suit, player_cards)
+        suit = Card.Suit.getShortStrSuit(action_str[-1])
+        mdp_card = Card(suit, int(action_str[:-1]))
+        if mdp_card not in self.cards:
+            raise Exception(
+                f"was not able to find mdp_card: ({mdp_card.suit},{mdp_card.num}) in cards: {self.cards})")
+            # choose a random card to essentially penalize for making an incorrect decision
+            # return next(iter(self.cards))
+        # print(
+        #     f"Agent {self.agent_id} played {str(mdp_card)} w/ hand {[str(card) for card in self.cards]}")
+        print(f"\nMDP CARD (best): {mdp_card}\n")
+        return mdp_card
+
+    def observeActionTaken(self, agent_id, card):
+        print(
+            f"observeActionTaken by agent {agent_id} and card {card.num} {card.suit} and in_play: {[str(card) for card in self.in_play]}")
+        self.observe_action_taken_fn(
+            f"{card.num}{Card.Suit.getSuitShortStr(card.suit)}")
+
+        if self.agent_id == agent_id:
+            self.cards.remove(card)
+        self.in_play.append(card)
+        if len(self.in_play) == self.NUM_AGENTS:
+            self.in_play.clear()
+
+    def resetSeenCards(self):
+        self.observe_action_taken_fn("reset")
---
+++ hearts/game_engine.py
@@ -0,0 +1,127 @@
+from card import Card
+from agent import SimpleHeartsAgent
+
+import random
+
+
+class HeartsEngine:
+    NUM_AGENTS = 4
+    QUEEN_OF_SPADES = Card(Card.Suit.SPADES, 12)
+    CARDS_PER_AGENT = Card.NUM_CARDS // NUM_AGENTS
+    
+
+    def __init__(self, agent_gen_fns):
+        self.agents_points = [0 for _ in range(self.NUM_AGENTS)]
+        assert len(agent_gen_fns) == self.NUM_AGENTS
+        self.agent_gen_fns = agent_gen_fns
+        self.in_deal = []
+
+    @classmethod
+    def create(cls):
+        return cls.createWithOneCustomAgent(lambda agent_id, cards: SimpleHeartsAgent(
+            agent_id, cards))
+
+    @classmethod
+    def createWithOneCustomAgent(cls, custom_agent_gen_fn):
+        agent_gen_fns = [lambda agent_id, cards: SimpleHeartsAgent(
+            agent_id, cards) for _ in range(cls.NUM_AGENTS - 1)]
+        agent_gen_fns.append(custom_agent_gen_fn)
+        return HeartsEngine(agent_gen_fns)
+
+    def deal(self):
+        print("deal")
+        self.in_deal = []
+        cards = [Card.createFromCardIdx(i) for i in range(Card.NUM_CARDS)]
+        random.shuffle(cards)
+        assert len(cards) % self.NUM_AGENTS == 0
+
+        # TODO
+        custom_agent_cards = [Card(Card.Suit.getShortStrSuit("H"),6), Card(Card.Suit.getShortStrSuit("D"), 14), Card(Card.Suit.getShortStrSuit("C"),12), Card(Card.Suit.getShortStrSuit("S"),2)] # read from Artur's output
+        print("DETECTED CARDS:")
+        for card in custom_agent_cards:
+            print((card.num, card.suit))
+
+        if len(custom_agent_cards) < self.CARDS_PER_AGENT:
+            needed_cards = self.CARDS_PER_AGENT - len(custom_agent_cards)
+            available_cards = [card for card in cards if card not in custom_agent_cards]
+            custom_agent_cards += random.sample(available_cards, needed_cards)
+        
+        available_cards = [card for card in cards if card not in custom_agent_cards]
+        assert len(available_cards) >= self.CARDS_PER_AGENT * (self.NUM_AGENTS - 1)
+
+        # self.agents = [self.agent_gen_fns[i](i,
+        #                                      cards[i * self.CARDS_PER_AGENT:(i + 1) * self.CARDS_PER_AGENT]) for i in range(self.NUM_AGENTS)]
+
+        agents_cards = [available_cards[i * self.CARDS_PER_AGENT:(i + 1) * self.CARDS_PER_AGENT] for i in range(self.NUM_AGENTS - 1)]
+        agents_cards.append(custom_agent_cards)
+
+        self.agents = [
+            self.agent_gen_fns[i](i, agents_cards[i]) for i in range(self.NUM_AGENTS)]
+
+    """
+    @returns winner agent_id of trick
+    """
+
+    def trick(self, start_agent_id):
+        print(f"trick with start: {start_agent_id}, points: {self.agents_points}")
+        in_trick = []  # (agent_id, Card)
+
+        # played = [Card(Card.Suit.getShortStrSuit("D"),3), Card(Card.Suit.getShortStrSuit("D"), 14), None] # read from Artur's output
+
+
+        for offset in range(self.NUM_AGENTS):
+            curr_agent_id = (start_agent_id + offset) % self.NUM_AGENTS
+            agent = self.agents[curr_agent_id]
+            card = agent.getNextAction()
+            # if curr_agent_id == 3:
+            #     card = agent.getNextAction()
+            # else:
+            #     if played[curr_agent_id] is not None:
+            #         card = played[curr_agent_id]
+            #     else:
+            #         card = agent.getNextAction()
+            
+            in_trick.append((curr_agent_id, card))
+            self.in_deal.append(card)
+            # notify all agents of a move
+            for notified_agent in self.agents:
+                notified_agent.observeActionTaken(agent.agent_id, card)
+        # update points based on winner of trick (ignores the rule reset rule when player takes all 13 hearts and the queen of spades, for sake of reduced complexity)
+        winner_agent_id = self._determine_trick_winner(in_trick)
+        won_points = 0
+        won_points += sum(
+            card.suit == Card.Suit.HEARTS for _, card in in_trick)
+        if self.QUEEN_OF_SPADES in map(lambda ac: ac[1], in_trick):
+            won_points += 13
+        print(
+            f"agent {winner_agent_id} won the trick and received {won_points} points")
+        self.agents_points[winner_agent_id] += won_points
+        return winner_agent_id
+
+    # @returns leaderboard: sorted (points, agent_id)
+    def play(self, win_points):
+        start_agent_id = 0
+        while True:
+            self.deal()
+            num_tricks_per_round = Card.NUM_CARDS // self.NUM_AGENTS
+            for i in range(num_tricks_per_round):
+                print(f"\n\nCalling trick for the {i}th time")
+                start_agent_id = self.trick(start_agent_id)
+                if any(map(lambda points: points >= win_points, self.agents_points)):
+                    leaderboard = sorted(
+                        [(points, i) for i, points in enumerate(self.agents_points)])
+                    for notified_agent in self.agents:
+                        notified_agent.resetSeenCards()
+                    return leaderboard
+
+    @staticmethod
+    def _determine_trick_winner(in_trick):
+        leading_suit = in_trick[0][1].suit
+        _, winner_agent_id = sorted(map(lambda agent_id_card: (
+            agent_id_card[1].num if agent_id_card[1].suit == leading_suit else float("-inf"), agent_id_card[0]), in_trick))[-1]
+        return winner_agent_id
+
+
+if __name__ == '__main__':
+    engine = HeartsEngine.create()
+    engine.play(50)
